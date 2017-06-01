a0a99770d848382626028ac0d303169140602830
Henrique Dias <heukirne@gmail.com>
2017-06-01 14:34:05 -0300

    update zip bash

--- zipBlogs.sh
+++ zipBlogs.sh
@@ -1,5 +1,8 @@
-gunzip blogs.tar.gz
-tar -uvf blogs.tar ./blogs/
-gzip -f blogs.tar
+echo 'Uncompressing Gzipped Tar file'
+#pv blogs.tar.gz | gunzip > blogs.tar
+echo 'Updating Tar file'
+tar -uf - ./blogs/ | pv > blogs.tar
+echo 'Compressing Tar file'
+pv blogs.tar | gzip > blog.tar.gz
 # tar -zcvf posts.csv.gz posts.csv
 echo 'Done'
